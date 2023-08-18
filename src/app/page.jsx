638f27da536900a1a71d7546ce78ba83319a0eb2
Rodrigo Menezes <rodrigo_menezes91@hotmail.com>
2023-08-18 15:29:02 -0300

    att

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -5,14 +5,17 @@ import { motion } from "framer-motion";
 export default function Home() {
   return (
     <div>
-      <Image src={cafe} alt="café" className="h-full absolute top-0 -z-10"/>
+      <Image src={cafe} alt="café" className="h-full absolute top-0 -z-10" />
       <motion.div
-      className="h-full flex flex-col justify-center items-center"
+       
+        className="h-full flex flex-col justify-center items-center drop-shadow-dark1"
       >
-          <p className="text-secondary text-4xl">O Poder do</p>
-          <h2 className="text-white text-9xl uppercase font-bold">Café</h2>
-          <p className="text-white uppercase">&#42; Desde 1930 &#42;</p>
-        </motion.div>
+        <p className=" text-4xl text-secondary">
+          O Poder do
+        </p>
+        <h2 className="text-white text-9xl uppercase font-bold">Café</h2>
+        <p className="text-white uppercase">&#42; Desde 1930 &#42;</p>
+      </motion.div>
     </div>
   )
 }
